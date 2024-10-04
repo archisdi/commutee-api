3fcff3a5543b045a3c02956ca531f473e73a6285
Archie Isdiningrat <archie.isdiningrat@cimbniaga.co.id>
2024-10-04 22:04:43 +0700

    update

--- index.js
+++ index.js
@@ -16,7 +16,27 @@ async function main() {
     const departureSchedule = { ...bekasiDeparture, ...cibuburDeparture };
 
     const dukuhAtasToJatimulya = lines[0].stations;
-    const jatimulyaToDukuhAtas = dukuhAtasToJatimulya.reverse();
+    // const jatimulyaToDukuhAtas = dukuhAtasToJatimulya.reverse();
+
+
+    const dukuhAtasToBekasiSchedule = {};
+
+    dukuhAtasToJatimulya.forEach((station, index) => {
+        const code = station.toLowerCase().trim().replace(/\s/g, '');
+
+        if (index === 0) {
+            dukuhAtasToBekasiSchedule[code] = {
+                station,
+                line: 'bekasi',
+                time: departureSchedule[code]
+            };
+            return;
+        }
+
+        const previousStation = dukuhAtasToJatimulya[index - 1].toLowerCase().trim().replace(/\s/g, '');
+    });
+
+    console.log(dukuhAtasToBekasiSchedule);
 }
 
 main();
